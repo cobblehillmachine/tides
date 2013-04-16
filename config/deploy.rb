17a32f4134ae52d4ceb4411db6fce80977dddce0
Andre Dublin <81dublin@gmail.com>
2013-04-16 10:39:33 -0400

    update
    Signed-off-by: Andre Dublin <81dublin@gmail.com>

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,16 +3,11 @@ set :repository,  "git@github.com:cobblehillmachine/tides.git"
 
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
-set :scm :git
-set :deploy_to "/var/www/apps/tides_beta/"
-set :deploy_via :remote_cache
+set :scm, :git
+set :deploy_to, "/var/www/apps/tides_beta/"
+set :deploy_via, :remote_cache
 set :copy_exclude, [".git", ".DS_Store", ".gitignore", ".gitmodules"]
 server "tides.cobblehilldigital.com", :app
-
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"

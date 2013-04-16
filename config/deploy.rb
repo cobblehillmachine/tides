08ec48f47db7224388b68abc8f362ef9e100c57b
Andre Dublin <81dublin@gmail.com>
2013-04-16 10:38:25 -0400

    capify;
    Signed-off-by: Andre Dublin <81dublin@gmail.com>

---
+++ config/deploy.rb
@@ -0,0 +1,30 @@
+set :application, "tides"
+set :repository,  "git@github.com:cobblehillmachine/tides.git"
+
+# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :scm :git
+set :deploy_to "/var/www/apps/tides_beta/"
+set :deploy_via :remote_cache
+set :copy_exclude, [".git", ".DS_Store", ".gitignore", ".gitmodules"]
+server "tides.cobblehilldigital.com", :app
+
+role :web, "your web-server here"                          # Your HTTP server, Apache/etc
+role :app, "your app-server here"                          # This may be the same as your `Web` server
+role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
+role :db,  "your slave db-server here"
+
+# if you want to clean up old releases on each deploy uncomment this:
+# after "deploy:restart", "deploy:cleanup"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
